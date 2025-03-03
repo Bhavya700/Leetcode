e086b96a73888ba96fc17fa0b6213fa4bebac46b
Bhavya Patel <116048013+Bhavya700@users.noreply.github.com>
2025-03-02 22:28:25 -0700

    Time: 0 ms (100%), Space: 8.4 MB (81.22%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open=0;
+        int close=0;
+        if (s.length()<1 || s.length()>1000){
+            return -1;
+        }
+        for (int i=0; i<s.length(); i++){
+            if (s[i]=='('){
+                open++;
+            }
+            else if (s[i]==')'){
+                if (open>0){
+                    open--;
+                }
+                else {
+                    close++;
+                }
+            }
+            else {
+                return -1;
+            }
+        }
+        return (close+open); 
+    }
+};
