8cbf9ae0b79a6ee98658cbf1271bb65fdb789a3d
Bhavya Patel <116048013+Bhavya700@users.noreply.github.com>
2025-03-03 17:19:00 -0700

    Time: 59 ms (29.63%), Space: 18.5 MB (9.1%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -0,0 +1,24 @@
+from collections import deque
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        R=[]
+        D=[]
+        n=len(senate)
+        for i in range(n):
+            if senate[i]=="R":
+                R.append(i)
+            elif senate[i]=="D":
+                D.append(i)
+        while R and D:
+            r=R.pop(0)
+            d=D.pop(0)
+            if r<d:
+                R.append(n+d)
+            else:
+                D.append(n+d)
+
+        return "Radiant" if R else "Dire"
+            
+        
+                
+
