2655cc8bae3dddbebbaec10084b7202b68266329
Bhavya Patel <116048013+Bhavya700@users.noreply.github.com>
2025-03-20 20:48:14 -0700

    Time: 8 ms (86.28%), Space: 18.5 MB (74.77%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        end=0
+        far=0
+        ans=0
+        n=len(nums)
+        for i in range(n-1):
+            far=max(far, i+nums[i])
+            if i == end:
+                ans+=1
+                end=far
+        return ans
+        
+
+
+        
+
+        
