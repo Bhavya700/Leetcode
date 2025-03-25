093b7906e9b769f0ea0eaadf7729aad84099debc
Bhavya Patel <116048013+Bhavya700@users.noreply.github.com>
2025-03-25 14:08:00 -0700

    Time: 3 ms (82.14%), Space: 17.8 MB (45.62%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -10,28 +10,21 @@ class Solution:
                 if grid[r][c]==1:
                     fresh+=1
                 elif grid[r][c]==2:
-                    q.append([r,c])
-        neighbours = [[0,1],[0,-1],[1,0],[-1,0]]
+                    q.append((r,c))
+        if fresh == 0:
+            return 0
+
+        neighbours = [(0,1),(0,-1),(1,0),(-1,0)]
         while q and fresh > 0:
             for i in range(len(q)):
                 r,c=q.popleft()
                 for dr, dc in neighbours:
                     row=dr+r
                     col=dc+c
-                    if (min(row,col)<0 or row==ROWS or col==COLS or grid[row][col]!=1):
-                        continue
-                    grid[row][col]=2
-                    q.append([row,col])
-                    fresh-=1
+                    if 0 <= row < ROWS and 0 <= col < COLS and grid[row][col] == 1:
+                        grid[row][col]=2
+                        q.append((row,col))
+                        fresh-=1
             time+=1
         
-        if fresh>0:
-            return -1
-        return time 
-
-
-
-
-
-
-
+        return time if fresh==0 else -1 
