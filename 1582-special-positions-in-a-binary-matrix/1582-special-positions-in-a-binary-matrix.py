a59fa79eb16425492a7aab59723b92ddb7056646
Bhavya Patel <116048013+Bhavya700@users.noreply.github.com>
2026-03-11 23:08:08 -0700

    Time: 2 ms (91.42%), Space: 20.1 MB (10.53%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import List
 
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
@@ -26,27 +27,6 @@ class Solution:
                         del seen[i]
                     else:
                         nc.add(val)
-                        del seen[i]
+            
         
         return len(nc-cols)
-
-
-
-        # res = 0
-        # m, n = len(mat), len(mat[0])
-        # row_sums = [sum(row) for row in mat]
-        # col_sums = [sum(row[i] for row in mat) for i in range(n)]
-        # for i in range(m):
-        #     if row_sums[i] > 1 or row_sums[i] == 0:
-        #         # we don't need to iterate over full matrix
-        #         # only need to consider valid rows/cols
-        #         continue  
-        #     for j in range(n):
-        #         if col_sums[j] > 1 or col_sums[j] == 0:
-        #             continue
-        #         is_special = (row_sums[i] + col_sums[j] - mat[i][j]) == 1 and mat[i][j] == 1
-        #         if is_special:
-        #             res += 1
-        # return res
-
-
