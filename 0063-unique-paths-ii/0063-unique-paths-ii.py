083a7d3676611a0e2952c9df03705e7f75412d8e
Bhavya Patel <116048013+Bhavya700@users.noreply.github.com>
2026-04-01 18:33:25 -0700

    Time: 0 ms (100%), Space: 19.4 MB (50.53%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -3,15 +3,15 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
-        prev = [0] * n
-        prev[0] = 1
+        dp = [0] * n
+        dp[0] = 1
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j]==1:
-                    prev[j] = 0
+                    dp[j] = 0
                 else:
                     if j>0:
-                        prev[j] += prev[j-1]
+                        dp[j] += dp[j-1]
         
-        return prev[-1]
+        return dp[-1]
