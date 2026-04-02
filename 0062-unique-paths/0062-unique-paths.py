ab3399e77685cadeb11e4c77659e0e67ed415252
Bhavya Patel <116048013+Bhavya700@users.noreply.github.com>
2026-04-01 17:28:33 -0700

    Time: 0 ms (100%), Space: 19.2 MB (84.34%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,3 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return math.comb(m+n-2,m-1)
+        prev = [1] * n
+
+        for i in range(m-1):
+            curr = [1] * n
+            for j in range(1, n):
+                curr[j] = curr[j-1] + prev[j]
+            prev = curr
+        
+        return prev[-1]
+
+
